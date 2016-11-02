ec95c10beabac3f773cf2e50894ff0841c6911ff
Jan Moxter <jan.moxter@innobix.com>
2016-11-02 10:28:20 -0500

    - Issue #15: Rebuild the EQL to use XCore and new testing
    adding tests for Query

--- src/test/java/org/efaps/eql/QueryTest.java
+++ src/test/java/org/efaps/eql/QueryTest.java
@@ -16,6 +16,12 @@
  */
 package org.efaps.eql;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -29,55 +35,108 @@ public class QueryTest
 
     /**
      * One type.
+     *
+     * @param _eqlBase the eql base
+     * @param _queryStmt the query stmt
      */
-    @Test(description = "query type Sales_Invoice")
-    public void oneType()
+    @Test(dataProvider = "PrintStmts", description = "type Sales_Invoice")
+    public void oneType(final String _eqlBase,
+                        final IQueryStmt<?> _queryStmt)
     {
-
+        _queryStmt.getQuery().addType("Sales_Invoice");
+        verifyStatement(_eqlBase + "type Sales_Invoice", _queryStmt);
     }
 
     /**
      * Two types.
+     *
+     * @param _eqlBase the eql base
+     * @param _queryStmt the query stmt
      */
-    @Test(description = "query type Sales_Invoice, Contacts_Contact")
-    public void twoTypes()
+    @Test(dataProvider = "PrintStmts", description = "type Sales_Invoice, Contacts_Contact")
+    public void twoTypes(final String _eqlBase,
+                         final IQueryStmt<?> _queryStmt)
     {
-
+        _queryStmt.getQuery().addType("Sales_Invoice").addType("Contacts_Contact");
+        verifyStatement(_eqlBase + "type Sales_Invoice, Contacts_Contact", _queryStmt);
     }
 
     /**
      * Many types.
+     *
+     * @param _eqlBase the eql base
+     * @param _queryStmt the query stmt
      */
-    @Test(description = "query type Sales_Invoice, Contacts_Contact, HumanResource_Employee")
-    public void manyTypes()
+    @Test(dataProvider = "PrintStmts", description = "type Sales_Invoice, Contacts_Contact,"
+                    + "HumanResource_Employee")
+    public void manyTypes(final String _eqlBase,
+                          final IQueryStmt<?> _queryStmt)
     {
+        _queryStmt.getQuery().addType("Sales_Invoice").addType("Contacts_Contact").addType("HumanResource_Employee");
+        verifyStatement(_eqlBase + "type Sales_Invoice, Contacts_Contact, HumanResource_Employee", _queryStmt);
     }
 
     /**
      * Type with number in name.
      *
+     * @param _eqlBase the eql base
+     * @param _queryStmt the query stmt
      * @throws Exception the exception
      */
-    @Test(description = "Type contains a number in the name")
-    public void typeWithNumberInName()
+    @Test(dataProvider = "PrintStmts", description = "Type contains a number in the name")
+    public void typeWithNumberInName(final String _eqlBase,
+                                     final IQueryStmt<?> _queryStmt)
         throws Exception
     {
+        _queryStmt.getQuery().addType("Sales_Invo2ice").addType("Contacts_Co4ntact");
+        verifyStatement(_eqlBase + "type Sales_Invo2ice, Contacts_Co4ntact", _queryStmt);
     }
 
     /**
      * Limit.
+     *
+     * @param _eqlBase the eql base
+     * @param _queryStmt the query stmt
      */
-    @Test(description = "query with a limit")
-    public void limit()
+    @Test(dataProvider = "PrintStmts", description = "query with a limit")
+    public void limit(final String _eqlBase,
+                      final IQueryStmt<?> _queryStmt)
     {
+        _queryStmt.getQuery().addType("Sales_Invoice").setLimitC(IEqlFactory.eINSTANCE.createLimit().setValueC("10"));
+        verifyStatement(_eqlBase + "type Sales_Invoice limit 10", _queryStmt);
     }
 
     /**
      * Type UUID.
+     *
+     * @param _eqlBase the eql base
+     * @param _queryStmt the query stmt
      */
-    @Test(description = "query type 0029a77d-65ca-4564-968c-2ef6adf72033, Contacts_Contact, "
-                    + "ce71ffa1-98f2-49b4-b892-246cd407b520, afc188bb-96a6-40ac-a48b-cfba985474c5")
-    public void typeUUID()
+    @Test(dataProvider = "PrintStmts", description = "type 0029a77d-65ca-4564-968c-2ef6adf72033, "
+                    + "Contacts_Contact, ce71ffa1-98f2-49b4-b892-246cd407b520, Sales_Invoice")
+    public void typeUUID(final String _eqlBase,
+                         final IQueryStmt<?> _queryStmt)
     {
+        _queryStmt.getQuery().addType("0029a77d-65ca-4564-968c-2ef6adf72033").addType("Contacts_Contact")
+            .addType("ce71ffa1-98f2-49b4-b892-246cd407b520").addType("Sales_Invoice");
+        verifyStatement(_eqlBase + "type 0029a77d-65ca-4564-968c-2ef6adf72033, Contacts_Contact, "
+                        + "ce71ffa1-98f2-49b4-b892-246cd407b520, Sales_Invoice", _queryStmt);
+    }
+
+
+    /**
+     * Ci commands.
+     *
+     * @param _context context
+     * @return iterator with ciform
+     */
+    @DataProvider(name = "PrintStmts")
+    public static Iterator<Object[]> printStmts(final ITestContext _context)
+    {
+        final List<Object[]> ret = new ArrayList<>();
+        ret.add(new Object[] { "print query ",
+                        IEqlFactory.eINSTANCE.createPrintQueryStatement()
+                    .setQueryC(IEqlFactory.eINSTANCE.createQuery())});
+        return ret.iterator();
     }
 }
