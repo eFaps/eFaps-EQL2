844443164379cf49d2e096ee2561119567e6a13b
Jan Moxter <jan.moxter@innobix.com>
2016-11-03 18:35:12 -0500

    - Issue #15: Rebuild the EQL to use XCore and new testing
    tests for insert

--- src/test/java/org/efaps/eql/InsertTest.java
+++ src/test/java/org/efaps/eql/InsertTest.java
@@ -26,14 +26,69 @@ import org.testng.annotations.Test;
 public class InsertTest
     extends AbstractTest
 {
-
     /**
      * Insert.
      */
-    @Test(description = "insert type")
+    @Test(description = "insert type EFaps_Type")
     public void insert()
     {
-        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement();
-        verifyStatement("insert type", stmt);
+        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement().typeName("EFaps_Type");
+        verifyStatement("insert type EFaps_Type", stmt);
+    }
+
+    /**
+     * Sets the attr num.
+     */
+    @Test(description = "insert type TYPE set ATTR=NUM")
+    public void setAttrNum()
+    {
+        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement().typeName("EFaps_Type")
+                        .update("Number", "2333");
+        verifyStatement("insert type EFaps_Type set Number = 2333", stmt);
+    }
+
+    /**
+     * Sets the many attr num.
+     */
+    @Test(description = "insert type TYPE  set ATTR=NUM,ATTR=NUM,ATTR=NUM")
+    public void setManyAttrNum()
+    {
+        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement().typeName("EFaps_Type")
+                        .update("Number", "2333").update("Number2", "111").update("Number3", "333");
+        verifyStatement("insert type EFaps_Type set Number = 2333, Number2 = 111, Number3 = 333", stmt);
+    }
+
+    /**
+     * Sets the attr str.
+     */
+    @Test(description = "insert type TYPE set ATTR=STR")
+    public void setAttrStr()
+    {
+        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement().typeName("EFaps_Type")
+                        .update("Name", "Hallo Welt");
+        verifyStatement("insert type EFaps_Type set Name = \"Hallo Welt\"", stmt);
+    }
+
+    /**
+     * Sets the many attr str.
+     */
+    @Test(description = "insert type TYPE set ATTR=Str, ATRR=STR2")
+    public void setManyAttrStr()
+    {
+        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement().typeName("EFaps_Type")
+                        .update("Name", "Hallo Welt").update("Name2", "Hallo Welt2");
+        verifyStatement("insert type EFaps_Type set Name = \"Hallo Welt\", Name2 = \"Hallo Welt2\"", stmt);
+    }
+
+    /**
+     * Sets the many attr str.
+     */
+    @Test(description = "insert type TYPE set ATTR=Str, Attr=Num, ATRR=STR2")
+    public void setManyAttrMixed()
+    {
+        final IEQLElement stmt = IEqlFactory.eINSTANCE.createInsertStatement().typeName("EFaps_Type")
+                        .update("Name", "Hallo Welt").update("Number", "333").update("Name2", "Hallo Welt2");
+        verifyStatement("insert type EFaps_Type set Name = \"Hallo Welt\", "
+                        + "Number = 333, Name2 = \"Hallo Welt2\"", stmt);
     }
 }
